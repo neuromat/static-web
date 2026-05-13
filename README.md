3a6ff5227d84e3f8484317a90b0af75d7d67a34d
Arthur Rodrigues <arthur@ime.usp.br>
2026-05-13 17:31:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Site estático Neuromat
+
+Esse repositório contém o código fonte da versão estática do site do [CEPID Neuromat](https://neuromat.numec.prp.usp.br).
---
+++ content/is-the-frequency-in-somatosensory-electrical-stimulation-the-key-parameter-in-modulating-the-corticospinal-excitability-of-healthy-volunteers-and-stroke-patients-with-spasticity.38f.d
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-6 [XR&CO'2014] -->
+
+<!-- Mirrored from neuromat.numec.prp.usp.br/content/is-the-frequency-in-somatosensory-electrical-stimulation-the-key-parameter-in-modulating-the-corticospinal-excitability-of-healthy-volunteers-and-stroke-patients-with-spasticity by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Oct 2025 13:10:41 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=is-the-frequency-in-somatosensory-electrical-st/index.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="is-the-frequency-in-somatosensory-electrical-st/index.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-6 [XR&CO'2014] -->
+
+<!-- Mirrored from neuromat.numec.prp.usp.br/content/is-the-frequency-in-somatosensory-electrical-stimulation-the-key-parameter-in-modulating-the-corticospinal-excitability-of-healthy-volunteers-and-stroke-patients-with-spasticity by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Oct 2025 13:10:41 GMT -->
+</HTML>
